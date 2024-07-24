35373e0c1c08a68081d9fbac4d9dd8be4a1e81dd
Bloodraven <74134064+Deshan555@users.noreply.github.com>
2024-07-24 23:35:49 +0530

    feat: Add error handling and logging to ruleService

--- src/services/ruleService.js
+++ src/services/ruleService.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { randomUUID } = require('crypto');
 const Rules = require('../models/ruleModel');
+const { errorResponse, successResponse } = require('../utils/response');
+const logger = require('../config/logger');
 
 router.post('/define/rule', async (req, res) => {
     const { projectName, sectionName, ruleName, ruleDescription, ruleType, ruleDefination, ruleStatus } = req.body;
@@ -9,13 +11,7 @@ router.post('/define/rule', async (req, res) => {
         let rule = await Rules.findOne({
             ruleName
         });
-        if (rule) {
-            return res.status(400).json({
-                errors: [{
-                    msg: 'Rule already exists'
-                }]
-            });
-        }
+        rule? errorResponse(res, 'Rule already exists', 400): null
         rule = new Rules({
             _id: randomUUID(),
             projectName,
@@ -27,17 +23,52 @@ router.post('/define/rule', async (req, res) => {
             ruleStatus
         });
         await rule.save();
-        res.send('Rule defined');
+        successResponse(res, 'Rule defined successfully', rule);
     } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+        logger.error(err.message);
+        errorResponse(res, 'Server Error', 500);
     }
 });
 
-// hello world endpoint
-router.get('/hello', (req, res) => {
-    res.send('Hello World!');
+router.get('/get/rules', async (req, res) => {
+    try {
+        const rules = await Rules.find();
+        successResponse(res, 'Rules fetched successfully', rules);
+    } catch (err) {
+        logger.error(err.message);
+        errorResponse(res, 'Server Error', 500);
+    }
 });
 
+router.get('/get/rule/:id', async (req, res) => {
+    try {
+        const rule = await Rules.findById(req.params.id);
+        rule? successResponse(res, 'Rule fetched successfully', rule): errorResponse(res, 'Rule not found', 400);
+    } catch (err) {
+        logger.error(err.message);
+        errorResponse(res, 'Server Error', 500);
+    }
+});
+
+// UPDATE SPECIFIC RULE BASED ON ID
+router.put('/update/rule/:id', async (req, res) => {
+    try {
+        const{ projectName, sectionName, ruleName, ruleDescription, ruleType, ruleDefination, ruleStatus } = req.body;
+        let rule = await Rules.findById(req.params.id);
+        rule? null: errorResponse(res, 'Rule not found', 400);
+        rule.projectName = projectName;
+        rule.sectionName = sectionName;
+        rule.ruleName = ruleName;
+        rule.ruleDescription = ruleDescription;
+        rule.ruleType = ruleType;
+        rule.ruleDefination = ruleDefination;
+        rule.ruleStatus = ruleStatus;
+        await rule.save();
+        successResponse(res, 'Rule updated successfully', rule);
+    } catch (err) {
+        logger.error(err.message);
+        errorResponse(res, 'Server Error', 500);
+    }
+});
 
 module.exports = router;
